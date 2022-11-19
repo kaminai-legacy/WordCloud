b4d0095e4cb52c6bfed2391a0ee9cd19a88f8af4
Olexander Osnova <alex.osnova.work@gmail.com>
2022-11-19 12:07:22 +0200

    init vue project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import DatasetManager from '@/components/DatasetManager.vue';
+import Filter from '@/components/Filter.vue';
+import WordCloud from '@/components/WordCloud.vue';
+</script>
+
+<template>
+  <header>
+    <h1>WordCloud</h1>
+    <nav>
+      <Filter/>
+    </nav>
+  </header>
+  <aside>
+    <DatasetManager/>
+  </aside>
+  <main>
+    <WordCloud />
+  </main>
+</template>
+
+<style scoped>
+</style>
