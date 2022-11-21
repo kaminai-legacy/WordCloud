530c9309a5031a1a95cac70127d97392808b7058
Olexander Osnova <alex.osnova.work@gmail.com>
2022-11-21 22:54:28 +0200

    add helper-functions add helper-functions for math and functions managing

---
+++ src/helpers/logicHelper.js
@@ -0,0 +1,18 @@
+export const debounce = (func, timeout = 300) => {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+}
+
+export const throttle = (func, delay) => {
+    let prev = 0;
+    return (...args) => {
+        let now = new Date().getTime();
+        if (now - prev > delay) {
+            prev = now;
+            return func(...args);
+        }
+    }
+}
---
+++ src/helpers/mathHelper.js
@@ -0,0 +1,1 @@
+export const toFloat = (n, f) => Math.round(n * 10 * f) / 10 * f;
